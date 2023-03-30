03309d4ab4dd25f70d0af08865115fae48342a6f
TGXD009 <129114183+TGXD009@users.noreply.github.com>
2023-03-30 10:09:57 +0200

    Update Quests.js

--- Quests.js
+++ Quests.js
@@ -15,3 +15,9 @@ document.addEventListener('click', ({target}) => {
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelectorAll('.quest').forEach(({dataset: { reward: r }}) => questClicks[r] = 0);
 });
+
+<div id='sword-quest' class='quest' data-count='5' data-reward='sword'>Click Me Lots !!</div>
+<div class='reward' id='sword'>Here is a sword</div>
+
+<div id='shield-quest' class='quest' data-count='3' data-reward='shield'>Click Me Lots !!</div>
+<div class='reward' id='shield'>Here is a sword</div>
