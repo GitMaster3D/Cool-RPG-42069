e830ab13b10325ffc6c2fc896d9eb243efe865c1
TGXD009 <129114183+TGXD009@users.noreply.github.com>
2023-03-30 10:20:01 +0200

    Update Level System.js

--- Level System.js
+++ Level System.js
@@ -6,9 +6,10 @@ function updatecounters()
     if (xp > levelxp) 
     {
         
-        level+=Math.floor(xp/levelxp);// if xp is 1000, two levels up
+        
         levelxp=(levelxp*2)/1,25;// je höher der Level desto mehr xp muss man leveln
         xp=0;//nach jeden levelup wird die exp anzahl auf 0 gesetzt
+        level++;
 
      }
      // Je höher der Level desto mehr XP braucht man
