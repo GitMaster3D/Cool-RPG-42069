88444e8eae02418a6e0325b77864f1daf78dcab2
GitMaster3D <115488492+GitMaster3D@users.noreply.github.com>
2023-05-02 09:03:11 +0200

    Add files via upload
    currentTiles variable, (3d array), enthält alle tiles nach Position (gerundet)

---
+++ Cool-RPG-42069-main/Audio.js
@@ -0,0 +1,17 @@
+
+//Load Event
+window.addEventListener('DOMContentLoaded', () => {
+    Init();
+});
+
+function Init()
+{
+    
+}
+
+// Plays sound witht the given name inside the "Audio" Folder
+function PlaySound(name = "Path")
+{
+    var sound = new Audio("/Audio/Sound/" + name);
+    sound.play();
+}
---
+++ Cool-RPG-42069-main/InputManager.js
@@ -0,0 +1,71 @@
+
+function InitInputManager()
+{
+    console.log("Initiated Input manager"); 
+}
+
+
+const upInputEvent = new Event("UpInput");
+const downInputEvent = new Event("DownInput");
+const rightInputEvent = new Event("RightInput");
+const leftInputEvent = new Event("LeftInput");
+const spaceInputEvent = new Event("SpaceInput");
+
+const upReleaseEvent = new Event("UpRelease");
+const downReleaseEvent = new Event("DownRelease");
+const rightReleaseEvent = new Event("RightRelease");
+const leftReleaseEvent = new Event("LeftRelease");
+const spaceReleaseEvent = new Event("SpaceRelease");
+
+function KeyPress(key)
+{
+    switch (key)
+    {
+        case "KeyA":
+            window.dispatchEvent(leftInputEvent);
+        break;
+
+        case "KeyD":
+            window.dispatchEvent(rightInputEvent);
+        break;
+
+        case "KeyW":
+            window.dispatchEvent(upInputEvent);
+        break;
+
+        case "KeyS":
+            window.dispatchEvent(downInputEvent);
+        break;
+
+        case "Space":
+            window.dispatchEvent(spaceInputEvent);
+        break;
+    }   
+}
+
+
+function KeyRelease(key)
+{
+    switch (key)
+    {
+        case "KeyA":
+            window.dispatchEvent(leftReleaseEvent);
+        break;
+
+        case "KeyD":
+            window.dispatchEvent(rightReleaseEvent);
+        break;
+
+        case "KeyW":
+            window.dispatchEvent(upReleaseEvent);
+        break;
+
+        case "KeyS":
+            window.dispatchEvent(downReleaseEvent);
+        break;
+
+        case "Space":
+            window.dispatchEvent(spaceReleaseEvent);
+        break;
+    }   
+}
