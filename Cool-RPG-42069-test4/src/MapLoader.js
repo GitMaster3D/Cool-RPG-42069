6a6674488f38ec974757b47c56e4cebbd4699aae
JJBro9090 <129147748+JJBro9090@users.noreply.github.com>
2023-06-06 09:12:41 +0200

    Add files via upload
    Spieler wird nun über den 2 Map-Layers angezeigt

---
+++ Cool-RPG-42069-test4/src/MapLoader.js
@@ -0,0 +1,59 @@
+async function extractVector2Arrays(name = "") {
+    path = "./assets/maps/" + name;
+    mapData = await GetTileset(path);
+
+    const tilePositions = [];
+    const tileIds = [];
+
+    let layer = mapData.layers[0];
+    const width = layer.width;
+    const height = layer.height;
+
+    for (let i = 0; i < layer.data.length; i++) {
+        const tileId = layer.data[i];
+        if (tileId !== 0) {
+            const x = i % width;
+            const y = Math.floor(i / width);
+            tilePositions.push([x, y]);
+            tileIds.push(tileId);
+
+            go = new GameObject(new Vector2(x, y), TileidToVec(tileId));
+        }
+    }
+
+    let layer2 = mapData.layers[1];
+    for (let i = 0; i < layer2.data.length; i++) {
+        const tileId = layer2.data[i];
+        if (tileId !== 0) {
+            const x = i % width;
+            const y = Math.floor(i / width);
+            tilePositions.push([x, y]);
+            tileIds.push(tileId);
+
+            go = new GameObject(new Vector2(x, y), TileidToVec(tileId));
+        }
+    }
+    
+}
+
+
+
+async function GetTileset(name = "") {
+    var response = await fetch(name);
+    return await response.json();
+}
+
+
+function TileidToVec(id= 0) {
+    var y = 0;
+    id--;
+
+    while (id > spriteSheetWidth)
+    {
+        id -= spriteSheetWidth;
+        y++;
+    }
+
+
+    return new Vector2(id | 0, y | 0 );
+}
