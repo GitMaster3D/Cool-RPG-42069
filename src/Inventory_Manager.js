7dd0832a2d7f4e05610e196ce44fe14298b34f92
NorisskNoFun <lukas6306@web.de>
2023-06-13 08:39:00 +0200

    Added Tileset

---
+++ src/Inventory_Manager.js
@@ -0,0 +1,10 @@
+class inventory_Manager//13|0-13|4
+{
+constructor(Slot1,Slot2,Slot3,Slot4,Slot5){
+    this.Slot1 =Slot1;
+    this.Slot2=Slot2;
+    this.Slot3=Slot3;
+    this.Slot4=Slot4;
+    this.Slot5=Slot5;
+}
+}
